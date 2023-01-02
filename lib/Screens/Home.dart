cce20a8f2bd36ab7d838d27875fa45c0498a877a
Stargazing-11 <samuelkifle199314@gmail.com>
2023-01-02 11:26:39 +0300

    Aded Home Screen

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,6 +1,12 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, sized_box_for_whitespace, unnecessary_null_comparison, prefer_if_null_operators, unnecessary_brace_in_string_interps
 
 import 'package:flutter/material.dart';
+import 'package:rahove/Screens/Components/bottomNavBAr.dart';
+import 'package:rahove/Screens/UserInfo.dart';
+import 'package:rahove/Screens/helpers/operations.dart';
+import 'package:rahove/Screens/helpers/scrollObject.dart';
+
+// import 'package:dotted_line/dotted_line.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,81 +15,208 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  final recents = [
+    [
+      "Yonas Tesfaye",
+      "+25193909908",
+      "+1000.2",
+      "2023-01-01 16:17",
+      "yonas.jpg"
+    ],
+    [
+      "Mihret Belay",
+      "+25193909908",
+      "+700.2",
+      "2023-01-01 16:17",
+      "mihret.jpg"
+    ],
+    [
+      "Soliana Abrha",
+      "+25193909908",
+      "+200.2",
+      "2023-01-01 16:17",
+      "soliana.jpg"
+    ],
+    [
+      "Dawit Tsige",
+      "+25193909908",
+      "+1000.2",
+      "2022-12-31 16:17",
+      "solomon.jpg"
+    ],
+    [
+      "Dawit Tsige",
+      "+25193909908",
+      "+1000.2",
+      "2022-12-31 16:17",
+      "solomon.jpg"
+    ],
+  ];
+  TabController? _controller;
+  TabController? _controller2;
+
+  int currentIndex = 0;
+  void initState() {
+    _controller2 =
+        TabController(length: 1, vsync: this, initialIndex: currentIndex);
+    _controller =
+        TabController(length: 4, vsync: this, initialIndex: currentIndex);
+  }
+
+  void changeTab(index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(210, 71, 118, 230),
-              Color.fromARGB(230, 142, 84, 233),
-            ],
-          ),
-        ),
-        child: Column(
+        extendBody: true,
+        body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(),
-                Text('Home'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text('Hello Soliana'),
-            Text('Let\'s check our status'),
             Container(
-              height: 220,
-              width: double.infinity,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
+              margin: EdgeInsets.only(bottom: 4),
+              padding: EdgeInsets.only(
+                left: 15,
+                top: 20,
+                right: 10,
+                bottom: 10,
+              ),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(210, 71, 118, 230),
+                    Color.fromARGB(230, 142, 84, 233),
+                  ],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    margin: EdgeInsets.all(5),
-                    height: 200,
-                    width: 300,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Color.fromARGB(255, 252, 252, 252)),
-                    child: Column(
-                      children: [
-                        Row(children: [
-                          Row(
-                            children: [
-                              Image.asset('assets/images/feres.png'),
-                              Column(
-                                children:[
-                                Text(
-                                  'Feres Transport'
-                                ),
-                                Text(
-                                  'Http://www.feres.com'
-                                )],
-                              )
-                            ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => UserInfo(),
+                            ),
+                          );
+                        },
+                        child: ClipRRect(
+                          child: Image.asset(
+                            'assets/images/liya.jpg',
+                            width: 50,
+                            height: 50,
                           ),
-                          IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz_rounded))
-                        ],
-                        )
-                      ],
+                          borderRadius: BorderRadius.circular(100.0),
+                        ),
+                      ),
+                      Text(
+                        'Home',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.now_widgets_outlined,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Hello Soliana',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Let\'s check our status',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 220,
+                    width: double.infinity,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: ((context, index) => container()),
                     ),
                   )
                 ],
               ),
-            )
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: TabBarView(
+                  controller: _controller2,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Recent Operations',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 151, 71, 255),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                          color: Color.fromRGBO(133, 133, 133, 1),
+                          child: Text(
+                            "Today",
+                            style: TextStyle(fontSize: 10),
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: recents.length,
+                            itemBuilder: ((context, index) => operation(
+                                recents[index][0],
+                                recents[index][1],
+                                recents[index][2],
+                                recents[index][3],
+                                picture: recents[index][4] != null
+                                    ? recents[index][4]
+                                    : null)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
-      ),
-    );
+        bottomNavigationBar: bottomNavBar(_controller));
   }
+}
+
+int dateDifference(date) {
+  return (DateTime.now().difference(DateTime.parse(date)).inHours / 24).round();
 }
